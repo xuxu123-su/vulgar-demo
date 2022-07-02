76140ea4514792026c086a3e8c106c21d51a3181
vulgar <1165709670@qq.com>
2022-07-02 21:50:40 +0800

    vistday

--- src/components/EchartSale/VisitDay.vue
+++ src/components/EchartSale/VisitDay.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <v-chart :option="option" style="height: 200px"/>
-  </div>
+  <div id="bar" style="height: 200px"></div>
 </template>
 
 <script>
@@ -9,31 +7,38 @@
     name: "VisitDay",
     data() {
       return {
-        option: {
-          xAxis: {
-            type: 'category',
-            data: ['00:00', '02:00', '04:00', '06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00'],
-            axisTick: {
-              alignWithLabel: true
-            }
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [
-            {
-              type: 'bar',
-              data: [10, 82, 20, 34, 30, 20, 36, 54, 35, 29, 15, 29],
-            }
-          ],
-          grid: {
-            top: 0,
-            bottom: 20,
-            left: 50,
-            right: 0
-          }
-        }
+        
       }
+    },
+
+    mounted() {
+      const charts = document.getElementById("bar")
+      const chart = this.$echarts.init(charts)
+      chart.setOption({
+        color: ['#91cc75'],
+        xAxis: {
+          type: 'category',
+          data: ['00:00', '02:00', '04:00', '06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            type: 'bar',
+            data: [10, 82, 20, 34, 30, 20, 36, 54, 35, 29, 15, 29],
+          }
+        ],
+        grid: {
+          top: 0,
+          bottom: 20,
+          left: 50,
+          right: 0
+        }
+      })
     }
   }
 </script>
